3491041008723484ba0b28fade24fd8588c4b9ed
Shoraki, Mohsen <Mohsen.Shoraki1@T-Mobile.com>
2018-11-07 12:44:14 -0600

    refactor on main class name.

--- transaction-app/src/main/java/edu/dcccd/TransactionApplication.java
+++ transaction-app/src/main/java/edu/dcccd/TransactionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TransactionAppApplication {
+public class TransactionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TransactionAppApplication.class, args);
+        SpringApplication.run(TransactionApplication.class, args);
     }
 
 }
